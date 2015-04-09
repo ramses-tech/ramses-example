81925f6f183792952646e3647d5ef5dc4e8bb1ba
Jonathan Stoikovitch <jstoiko@users.noreply.github.com>
2015-04-09 17:45:54 -0400

    merging with https://github.com/brandicted/ramses-example/pull/5

--- ramses_example/__init__.py
+++ ramses_example/__init__.py
@@ -1,10 +1,7 @@
 from pyramid.config import Configurator
-from wsgiref.simple_server import make_server
 
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('ramses')
-    app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', 8080, app)
-    server.serve_forever()
+    return config.make_wsgi_app()
