e6437ada1674b8d13da896d03553a3c4cd5d70eb
Jonathan Stoikovitch <jstoiko@users.noreply.github.com>
2015-04-09 17:32:53 -0400

    Revert "Use Paste, waitress. Update ini template. Use new ID fields from engines"

--- ramses_example/__init__.py
+++ ramses_example/__init__.py
@@ -1,7 +1,10 @@
 from pyramid.config import Configurator
+from wsgiref.simple_server import make_server
 
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('ramses')
-    return config.make_wsgi_app()
+    app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 8080, app)
+    server.serve_forever()
