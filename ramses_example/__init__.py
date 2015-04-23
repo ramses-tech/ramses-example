716041c497c15f53586905a4d344dfc79a9aeaf8
Artem Kostiuk <postatum@gmail.com>
2015-04-23 14:09:14 +0300

    Update RAML security schemas. Define `user_himself` ACL handler

--- ramses_example/__init__.py
+++ ramses_example/__init__.py
@@ -4,11 +4,16 @@ from ramses import registry
 
 @registry.add
 def user_himself(ace, request, obj):
+    """ Give 'patch' permission to user when he tries to edit
+    his own User object.
+    """
     from pyramid.security import Allow
-    return [
-        (Allow, str(obj.id), 'patch'),
-        (Allow, str(obj.username), 'patch'),
-    ]
+    user = getattr(request, 'user', None)
+    if user is not None and user.username == obj.username:
+        return [
+            (Allow, str(user.id), 'patch'),
+            (Allow, str(user.username), 'patch'),
+        ]
 
 
 def main(global_config, **settings):
